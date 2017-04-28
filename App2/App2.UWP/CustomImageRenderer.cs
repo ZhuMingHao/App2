e735d45650b0fe1c2e15bf862f153dae0ba222a3
v-zhumin <v-zhumin@V-ZHUMIN-W10>
2017-04-28 17:42:49 +0800

    Add project files.

---
+++ App2/App2.UWP/CustomImageRenderer.cs
@@ -0,0 +1,48 @@
+﻿using App2;
+using App2.UWP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(CustomImage), typeof(CustomImageRenderer))]
+
+namespace App2.UWP
+{
+    internal class CustomImageRenderer : ImageRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Image> e)
+        {
+            base.OnElementChanged(e);
+            if (e.NewElement != null)
+            {
+                var image = new Windows.UI.Xaml.Controls.Image();
+                image.ImageOpened += Image_ImageOpened; ;
+                SetNativeControl(image);
+            }
+        }
+
+        public override SizeRequest GetDesiredSize(double widthConstraint, double heightConstraint)
+        {
+            if (Control.Source == null)
+                return new SizeRequest();
+
+            var result = new Size { Width = ((BitmapSource)Control.Source).PixelWidth, Height = ((BitmapSource)Control.Source).PixelHeight };
+
+            if (result != null)
+            {
+                ((IImageController)Element)?.SetIsLoading(false);
+            }
+            return new SizeRequest(result);
+        }
+
+        private void Image_ImageOpened(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            ((IImageController)Element)?.SetIsLoading(false);
+        }
+    }
+}
---
+++ App2/App2/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Img_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+        }
+    }
+
+    public class imagesource : ImageSource
+    {
+        public imagesource()
+        {
+        }
+    }
+}
